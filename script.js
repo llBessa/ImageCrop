50cd5bf616ffd37c0e30f219259a51966d4aaa93
llBessa <82344911+llBessa@users.noreply.github.com>
2022-02-25 15:51:41 -0400

    funcionalidades finalizadas e alteração no readme

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const photoFile = document.getElementById('photo-file')
 let photoPreview = document.getElementById('photo-preview')
 
-let image = new Image()
-
+let image;
+let photoName;  // nome do arquivo aberto
+let photoExt;   // extensão do arquivo
 // Select & preview image
 
 document.getElementById('select-image')
@@ -13,11 +14,17 @@ document.getElementById('select-image')
 window.addEventListener('DOMContentLoaded', () => {
     photoFile.addEventListener('change',() => {
         let file = photoFile.files.item(0)
+
+        photoName = file.name.split('.')[0]
+        photoExt = file.name.split('.')[1]
+
         //ler um arquivo
         let reader = new FileReader()
         reader.readAsDataURL(file)
         reader.onload = function (event) {
-            image.src = event.target.result
+            image = new Image();
+            image.src = event.target.result;
+            image.onload = onLoadImage;
         }
     })
 })
@@ -62,6 +69,9 @@ const events = {
 
         relativeEndX = event.layerX
         relativeEndY = event.layerY
+
+        // mostrar o botão de corte
+        cropButton.style.display = 'initial'
     }
 }
 
@@ -75,16 +85,80 @@ Object.keys(events)
 let canvas = document.createElement('canvas')
 let ctx = canvas.getContext('2d')
 
-image.onload = function() {
+function onLoadImage() {
     const {width, height} = image
     canvas.width = width
     canvas.height = height
     
     // limpa o contexto
-    ctx.clearRect(0,0,width, height)
+    ctx.clearRect(0,0, width, height)
 
     // desenha a imagem
     ctx.drawImage(image, 0, 0)
 
     photoPreview.src = canvas.toDataURL()
+}
+
+//Crop
+const cropButton = document.getElementById('crop-image')
+cropButton.onclick = () => {
+    const {width : imgW, height: imgH} = image;
+    const {width : previewW, height : previewH} = photoPreview;
+
+    // guarda os fatores de altura e largura
+    const [widthFactor, heightFactor] = [
+        +(imgW / previewW),
+        +(imgH / previewH)
+    ]
+
+    // altura e largura da seleção
+    const [selectionW, selectionH] = [
+        +(selection.style.width).replace('px',''),
+        +(selection.style.height).replace('px','')
+    ]
+
+    // altura e largura do corte real
+    const [croppedW, croppedH] = [
+        +(selectionW*widthFactor),
+        +(selectionH*heightFactor)
+    ]
+
+    // posições X e Y da seleção
+    const [actualX, actualY] = [
+        +(relativeStartX*widthFactor),
+        +(relativeStartY*heightFactor)
+    ]
+
+    // pegar imagem cortada do contexto do canvas
+    const croppedImage = ctx.getImageData(actualX, actualY, croppedW, croppedH)
+
+    // limpar o contexto
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    // ajuste de proporção
+    image.width = canvas.width = croppedW;
+    image.height = canvas.height = croppedH;
+
+    //adicionar imagem cortada ao ctx
+    ctx.putImageData(croppedImage, 0, 0)
+
+    //esconder a ferramenta de seleção
+    selection.style.display = 'none'
+
+    // atualizar o preview da imagem
+    photoPreview.src = canvas.toDataURL()
+
+    //exibir botão de download
+    downloadButton.style.display = 'initial'
+}
+
+// Download
+const downloadButton = document.getElementById('download')
+
+// funcionalidade de download
+downloadButton.onclick = () => {
+    const a = document.createElement('a')
+    a.download = photoName + '-cropped.' + photoExt
+    a.href = canvas.toDataURL()
+    a.click()
 }
